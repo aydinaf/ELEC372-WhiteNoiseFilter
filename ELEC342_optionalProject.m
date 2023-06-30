6ebc53b34b259137a69099c0f43602e817ee0117
Aydin Azari Farhad <33768239+aydinaf@users.noreply.github.com>
2023-06-29 22:34:24 -0400

    Update ELEC342_optionalProject.m

--- ELEC342_optionalProject.m
+++ ELEC342_optionalProject.m
@@ -8,23 +8,24 @@ audio = resample(audio, targetFs, fs);
 duration = 60;
 numSamples = duration * targetFs;
 audio = audio(1:numSamples);
-% sound(audio, targetFs);
-% snrArray = [1,5,10,15];
-% for i = 1:length(snrArray)
-%     snr = snrArray(i);
-snr = 15;
-noisy = awgn(audio, snr, 'measured');
+noise = rand(size(audio));
+noise = highpass(noise, 400, targetFs);
+noisePwr = 5.01;
+noisy = audio + noise * noisePwr;
 audio_fft = fft(audio);
 noisy_fft = fft(noisy);
 soundsc(noisy,targetFs);
 figure;
-subplot(4,1,1);
+subplot(5,1,1);
 plot(linspace(0, length(audio)/targetFs, length(audio)),audio);
-subplot(4,1,2);
+subplot(5,1,2);
 plot(linspace(0, length(audio)/targetFs, length(audio)),noisy);
-subplot(4,1,3);
+subplot(5,1,3);
 plot(abs(audio_fft));
-subplot(4,1,4);
+subplot(5,1,4);
 plot(abs(noisy_fft));
+subplot(5,1,5);
+plot(abs(noisy_fft - audio_fft));
+
 
 
