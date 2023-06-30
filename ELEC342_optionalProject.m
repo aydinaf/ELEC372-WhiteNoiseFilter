4f1451a36ec87595e60a69f84dc2811535dd9e13
Aydin Azari Farhad <33768239+aydinaf@users.noreply.github.com>
2023-06-29 21:05:38 -0400

    first commit.
    Reads an imported audio file and samples the first 60 seconds at 8k Hz, adds white gaussian noise, plots original and distorted audio vs time.

---
+++ ELEC342_optionalProject.m
@@ -0,0 +1,18 @@
+inputFile = 'G:\My Documents\MATLAB\sampleAudio.mp3';
+[audio,fs] = audioread(inputFile);
+targetFs = 8000;
+audio = resample(audio, targetFs, fs);
+duration = 60;
+numSamples = duration * targetFs;
+audio = audio(1:numSamples);
+% sound(audio, targetFs);
+snr = 10;
+noisy = awgn(audio, snr, 'measured');
+soundsc(noisy,targetFs);
+figure;
+subplot(2,1,1);
+plot(linspace(0, length(audio)/targetFs, length(audio)),audio);
+subplot(2,1,2);
+plot(linspace(0, length(audio)/targetFs, length(audio)),noisy);
+
+
